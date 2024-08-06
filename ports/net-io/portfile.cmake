4222b8a35ba94124fa4e1353db95a775ef8052e2
dev <kirill@Kirills-MacBook-Air.local>
2024-08-06 22:30:43 +0300

    version update

--- ports/net-io/portfile.cmake
+++ ports/net-io/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO kirill-nn/net-io
         REF "${VERSION}"
-        SHA512 0
+        SHA512 188a5951b6db4dcd5e4d74d8f5ce40d84fc783b238c47360c9efb5e27899007685f33cde64f9d367340974fc290a10bb554cdf5d40686c15fdbb7334b760900a
         HEAD_REF master
 )
 
